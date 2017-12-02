0246cd1d8878e41d691fb99681314210accdc12c
adham bishr <adhambishr@gmail.com>
2017-12-02 11:35:33 -0800

    changes made

--- routes/routes.js
+++ routes/routes.js
@@ -17,39 +17,53 @@ router.get("/search/:email", function(req, res) {
 });
 
 router.get("/search/:email", function(req, res) {
-    console.log("Target Data:");
-    console.log(req.body);
-    Target.create({
-      name: req.body.target.name,
-      email: req.body.target.email,
-      employmentCompany: req.body.target.employmentCompany,
-      employmentTitle: req.body.target.employmentTitle,
-      linkedInURL: req.body.target.linkedInURL,
-      twitterHandle: req.body.target.twitterHandle,
-      location: req.body.target.location,
-      biography: req.body.target.biography
-    });
+  console.log("Target Data:");
+  console.log(req.body);
+  Target.create({
+    name: req.body.target.name,
+    email: req.body.target.email,
+    employmentCompany: req.body.target.employmentCompany,
+    employmentTitle: req.body.target.employmentTitle,
+    linkedInURL: req.body.target.linkedInURL,
+    twitterHandle: req.body.target.twitterHandle,
+    location: req.body.target.location,
+    biography: req.body.target.biography
+  }).catch(function(err) {
+    // Whenever a validation or flag fails, an error is thrown
+    // We can "catch" the error to prevent it from being "thrown", which could crash our node app
+    res.json(err);
   });
+});
 
-  router.get("/search/:email", function(req, res) {
-      console.log("Company Data:");
-      console.log(req.body);
-      Company.create({
-        companyName: req.body.company.companyName
-        companyFounded: req.body.company.companyFounded,
-        companyURL: req.body.company.companyURL,
-        companyBio: req.body.company.companyBio,
-        // companyEmails: req.body.company.companyEmails,
-        companyPhone: req.body.company.companyPhone,
-        companyCrunchbase: req.body.company.companyCrunchbase,
-        amountRaised: req.body.company.amountRaised,
-        revenue: req.body.company.revenue,
-        logo: req.body.company.logo
-      });
-    });
-    router.get("/search/:email", function(req, res) {
-        email: req.body.company.companyEmails,
-        companyName:req.body.company.companyName
-    });
+router.get("/search/:email", function(req, res) {
+  console.log("Company Data:");
+  console.log(req.body);
+  Company.create({
+    companyName: req.body.company.companyName,
+    companyFounded: req.body.company.companyFounded,
+    companyURL: req.body.company.companyURL,
+    companyBio: req.body.company.companyBio,
+    // companyEmails: req.body.company.companyEmails,
+    companyPhone: req.body.company.companyPhone,
+    companyCrunchbase: req.body.company.companyCrunchbase,
+    amountRaised: req.body.company.amountRaised,
+    revenue: req.body.company.revenue,
+    logo: req.body.company.logo
+  }).catch(function(err) {
+    // Whenever a validation or flag fails, an error is thrown
+    // We can "catch" the error to prevent it from being "thrown", which could crash our node app
+    res.json(err);
+  });
+});
+router.get("/search/:email", function(req, res) {
+  CompanyEmail.create({
+    email: req.body.company.companyEmails,
+    companyName: req.body.company.companyName
+  }).catch(function(err) {
+    // Whenever a validation or flag fails, an error is thrown
+    // We can "catch" the error to prevent it from being "thrown", which could crash our node app
+    res.json(err);
+  });
+});
 
 module.exports = router;
