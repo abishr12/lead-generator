e032be2926d338a28333f108f38310de7bac53e2
adham bishr <adhambishr@gmail.com>
2017-12-02 11:21:04 -0800

    separating modules

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,7 @@ router.get("/", function(req, res) {
 });
 
 router.get("/search/:email", function(req, res) {
-  //console.log(req.body.email);
+  // console.log(req.body.email);
 
   email = req.params.email;
 
@@ -15,5 +15,41 @@ router.get("/search/:email", function(req, res) {
     res.json(data);
   });
 });
+
+router.get("/search/:email", function(req, res) {
+    console.log("Target Data:");
+    console.log(req.body);
+    Target.create({
+      name: req.body.target.name,
+      email: req.body.target.email,
+      employmentCompany: req.body.target.employmentCompany,
+      employmentTitle: req.body.target.employmentTitle,
+      linkedInURL: req.body.target.linkedInURL,
+      twitterHandle: req.body.target.twitterHandle,
+      location: req.body.target.location,
+      biography: req.body.target.biography
+    });
+  });
+
+  router.get("/search/:email", function(req, res) {
+      console.log("Company Data:");
+      console.log(req.body);
+      Company.create({
+        companyName: req.body.company.companyName
+        companyFounded: req.body.company.companyFounded,
+        companyURL: req.body.company.companyURL,
+        companyBio: req.body.company.companyBio,
+        // companyEmails: req.body.company.companyEmails,
+        companyPhone: req.body.company.companyPhone,
+        companyCrunchbase: req.body.company.companyCrunchbase,
+        amountRaised: req.body.company.amountRaised,
+        revenue: req.body.company.revenue,
+        logo: req.body.company.logo
+      });
+    });
+    router.get("/search/:email", function(req, res) {
+        email: req.body.company.companyEmails,
+        companyName:req.body.company.companyName
+    });
 
 module.exports = router;
