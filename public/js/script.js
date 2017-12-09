e62028784e98519171a320662350e19ede12c4bf
donaldvallejo <donny.vallejo@gmail.com>
2017-12-08 16:24:50 -0800

    added place holder div and fixed templates

--- public/js/script.js
+++ public/js/script.js
@@ -3,12 +3,13 @@ $(document).ready(function(e) {
   // Load up saved emails when the page is loaded.
   // ===
   var initInterfaceData = function() {
-    var containerElement = $('#temporary-placeholder');
-    $.get('/api/emails').done(function(responseHTML) {                // GET call to fetch all emails. match the url with the one in your controller.
-      containerElement.html(responseHTML);                            // responseHTML should be a partial handlebars template
-    });
+    if (document.location.href.indexOf('/api/') !== -1) {
+      var containerElement = $('#temporary-placeholder');
+      $.get('/api/emails').done(function(responseHTML) {                // GET call to fetch all emails. match the url with the one in your controller.
+        containerElement.html(responseHTML);                            // responseHTML should be a partial handlebars template
+      });
+    }
   };
-
   initInterfaceData();                                                // initialize the ux by calling our helper function
 
   // ===                                                              // add a header to your event bindings so it's more understandable
@@ -19,17 +20,10 @@ $(document).ready(function(e) {
 
     var userSubmittedEmail = $("#top-search").val().trim();
     var url = "/api/search/" + userSubmittedEmail;                          // build the url with the encoded email address.
-    console.log(url);
-    $.get(url).done(function(data) {
-      console.log("data in script.js: ", data);
-      // do the stuff you need to do to render here
 
-      // changed: note that this must be the last call within the scope of $.get
-
-      // do not uncomment this until you are injecting HTML into the DOM.
-      // it will only cause headaches and confusion if you try to uncomment it now.
-
-      // location.reload();
+    $.get(url).done(function(responseHtml) {
+      console.log("rendered response: ", responseHtml);
+      $('#search-results-container').html(responseHtml);
     });
   });
 
