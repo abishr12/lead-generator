c9c6759da6fd1752409b0a20655d28afa0b608cd
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-12-16 00:01:28 -0800

    Logo, nav, settings, icons on cards

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
 
     let URL = `/api/search/${emailInput}/${userId}`;
 
-    $.get(URL).done(response => {
+    $.get(URL).done((response) => {
       console.log(response);
       // Convert the renderedPartial String to a jQuery object.
       let newTarget = $(response.renderedPartial);
@@ -23,7 +23,7 @@ $(document).ready(function () {
 
       newTarget.addClass("fadeInDown");
       //Add to the emails side bar with prepend()
-      $("#searched-names").prepend(newTarget);
+      $("span#saved-targets").prepend(newTarget);
     });
   });
 
@@ -32,7 +32,7 @@ $(document).ready(function () {
   $("li.list-group-item").off("click");
 
   // Reattach click handler to existing and the dynamically created list elements
-  $("ul#searched-names").on("click", ".list-group-item a", (event) => {
+  $("span#saved-targets").on("click", ".list-group-item a", (event) => {
     // Stop the bubbling effect of the click handler
     event.stopPropagation();
     // Prevent click on <a> from navigating to a new page
@@ -40,25 +40,91 @@ $(document).ready(function () {
     // Remove the active class from any li.active
     $(".nav-pf-vertical .list-group-item.active").removeClass("active");
     // Find the closest li.list-group-item to the click and add the active class
-    $(event.target).closest("li.list-group-item").addClass("active");
-    $("#contact-area").html("");
-    renderPanels()
+    let liClicked = $(event.target).closest("li.list-group-item");
+    // Store the email of the clicked target in a variable
+    let targetEmailAddress = liClicked.data("email");
+    // Add the active class to the clicked target
+    liClicked.addClass("active");
+    // Update the page with the clicked target's information
+    getSavedTartget(targetEmailAddress, renderPanels);
   });
 
+  function getSavedTartget(targetEmail, callback) {
+    $.get(`api/savedsearch/${targetEmail}`).done((targetResponse) => {
+      callback(targetResponse);
+    });
+  }
 
-  function renderPanels() {
+  function renderPanels(targetResponse) {
+
+    let targetHTML = ``;
+    let companyHTML = ``;
+
+    // Build HTML block based on information available in target record
+    if (targetResponse.target.name) {
+      targetHTML +=
+        `<button type="button" class="list-group-item">
+        <i class="fa fa-user" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;${targetResponse.target.name}
+      </button>`
+    }
+    if (targetResponse.target.employmentTitle) {
+      targetHTML += `<button type="button" class="list-group-item"><i class="fa fa-briefcase" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;${targetResponse.target.employmentTitle}</button>`
+    } else {
+      targetHTML += `<button type="button" class="list-group-item">Job title not available</button>`
+    }
+    if (targetResponse.target.email) {
+      targetHTML += `<a href="mailto:${targetResponse.target.email}"><button type="button" class="list-group-item">
+        <i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;${targetResponse.target.email}
+      </button></a>`
+    }
+    if (targetResponse.target.linkedInURL) {
+      targetHTML +=
+        `<button href="${targetResponse.target.linkedInURL}" target="_blank" type="button" class="list-group-item" >
+          <i class="fa fa-linkedin-square" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;LinkedIn Profile
+        </button>`
+    }
+    if (targetResponse.target.location) {
+      targetHTML +=
+      `<button type="button" class="list-group-item">
+        <i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;${targetResponse.target.location}
+      </button>`
+    }
+    if (targetResponse.target.twitterHandle) {
+      targetHTML +=
+        `<a href="https://www.twitter.com/${targetResponse.target.twitterHandle}" target="_blank">
+          <button type="button" class="list-group-item" >
+            <i class="fa fa-twitter" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;@${targetResponse.target.twitterHandle}
+          </button>
+      </a>`
+    }
+
+    // Build HTML block based on information available in company record
+    if (targetResponse.company.companyName) {
+      companyHTML +=
+      `<button type="button" class="list-group-item">${targetResponse.company.companyName}</button>`
+    }
+    if (targetResponse.company.companyFounded) {
+      companyHTML += `<button type="button" class="list-group-item">Est. ${targetResponse.company.companyFounded}</button>`
+    }
+    if (targetResponse.company.companyURL) {
+      companyHTML +=
+        `<a href="${targetResponse.company.companyURL}" target="_blank">
+        <button type="button" class="list-group-item">${targetResponse.company.companyURL}</button>
+      </a>`
+    }
+
+    // Clear the currently selected target
+    $("#contact-area").html("");
+    // Update the page with a new target's information
     $("#contact-area").html(`
     <div class="col-xs-12 col-sm-8 col-md-8 fadeInUp">
 			<div class="panel panel-primary">
 				<div class="panel-heading">
-					<h3 class="panel-title">Panel title</h3>
+					<h3 class="panel-title">Lead Information</h3>
 				</div>
 				<div class="panel-body">
-					<div class="list-group">
-						<button type="button" class="list-group-item">Jeff Lowy</button>
-						<button type="button" class="list-group-item">jlowy@sfaf.org</button>
-						<button type="button" class="list-group-item">Technical Operations Manager</button>
-						<button type="button" class="list-group-item">@jeffreylowy</button>
+          <div class="list-group">
+						${targetHTML}
 					</div>
 				</div>
 			</div>
@@ -66,15 +132,15 @@ $(document).ready(function () {
 		<div class="col-xs-12 col-sm-4 col-md-4 fadeInUp">
 			<div class="panel panel-default">
 				<div class="panel-heading">
-					<h3 class="panel-title">Panel title</h3>
+					<h3 class="panel-title">Company Information</h3>
 				</div>
 				<div class="panel-body">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quasi amet corrupti possimus soluta. Laboriosam exercitationem
-					voluptatem quaerat unde modi, suscipit ea, aut magni repellendus iure illum, repudiandae cum aperiam!
+					<div class="list-group">						
+						${companyHTML}
+					</div>
 				</div>
 			</div>
 		</div>
-    `)
+    `);
   }
-
 });
